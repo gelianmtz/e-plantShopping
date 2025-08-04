54d003b6a42ca9174b6ec411875bd31f4fc97191
gelianmtz <ger.elian.martinez@outlook.com>
2025-08-04 17:32:36 -0600

    ProductList component Layout

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -3,19 +3,13 @@ import { createSlice } from '@reduxjs/toolkit';
 export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
-    items: [], // Initialize items as an empty array
+    items: [] // Initialize items as an empty array
   },
   reducers: {
-    addItem: (state, action) => {
-    
-    },
-    removeItem: (state, action) => {
-    },
-    updateQuantity: (state, action) => {
-
-    
-    },
-  },
+    addItem: (state, action) => {},
+    removeItem: (state, action) => {},
+    updateQuantity: (state, action) => {}
+  }
 });
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
